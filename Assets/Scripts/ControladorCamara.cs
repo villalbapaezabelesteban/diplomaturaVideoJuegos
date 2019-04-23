2e025989023f62b4c24b2200e7a04b70f7a0b365
Abel Esteban Villalba Paez <villalbapaez.abelesteban@gmail.com>
2019-04-23 20:30:08 -0300

    Initial commit

---
+++ Assets/Scripts/ControladorCamara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorCamara : MonoBehaviour
+{
+    private GameObject playerAbel;
+    private Vector3 desplazamiento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAbel = GameObject.FindWithTag("PlayerAbel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ControladorPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorPlayer : MonoBehaviour
+{
+    float movimientoNeutro = 0.0f;
+
+    public float movimientoHorizontal = 0.1f;
+    public float movimientoVertical = 0.1f;
+
+    public float fuerzaMovimiento = 1.0f;
+
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        if (rigidbody != null)
+        {
+            bool izquierda = Input.GetKey("left");
+            bool derecha = Input.GetKey("right");
+            bool arriba = Input.GetKey("up");
+            bool abajo = Input.GetKey("down");
+
+            if (arriba)
+            {
+                rigidbody.AddForce(Vector3.forward * fuerzaMovimiento);
+            }
+
+            if (abajo)
+            {
+                rigidbody.AddForce(Vector3.back * fuerzaMovimiento);
+            }
+
+            if (izquierda)
+            {
+                rigidbody.AddForce(Vector3.left * fuerzaMovimiento);
+            }
+
+            if (derecha)
+            {
+                rigidbody.AddForce(Vector3.right * fuerzaMovimiento);
+            }
+        }
+    }
+}
